6102cc01819c85311f65ffb186bec2830ef064d8
maxterdize <maxter.rojas@gmail.com>
2024-08-21 17:31:48 -0600

    Adding service methods for cruds.

--- src/main/java/com/movies_service/movies_service_demo/service/MoviesService.java
+++ src/main/java/com/movies_service/movies_service_demo/service/MoviesService.java
@@ -2,6 +2,7 @@ package com.movies_service.movies_service_demo.service;
 
 import com.movies_service.movies_service_demo.client.MaverikClient;
 import com.movies_service.movies_service_demo.exceptions.MovieNotFoundException;
+import com.movies_service.movies_service_demo.exceptions.RepositoryTransactionException;
 import com.movies_service.movies_service_demo.model.Movie;
 import com.movies_service.movies_service_demo.repository.MovieRepository;
 import jakarta.transaction.Transactional;
@@ -30,9 +31,35 @@ public class MoviesService {
             movieRepository.saveAll(movies);
             movieRepository.flush();
         } catch (Exception e) {
-            throw new RuntimeException("Error saving movies with title: " + title, e);
+            throw new RepositoryTransactionException("Error saving movies with title: " + title, e);
         }
         return movies;
+    }
+
+    @Transactional
+    public void saveMovies(Set<Movie> movies) {
+        try {
+            movieRepository.saveAll(movies);
+        } catch (Exception e) {
+            throw new RepositoryTransactionException("Error saving list of movies", e);
+        }
+    }
+
+    @Transactional
+    public void saveMovie(Movie movie) {
+        try {
+            movieRepository.save(movie);
+        } catch (Exception e) {
+            throw new RepositoryTransactionException("Error saving movie with title: " + movie.getTitle(), e);
+        }
+    }
+
+    public Movie getMovieByIbmdId(String imdbId) {
+        Movie movie = movieRepository.findByImdbId(imdbId);
+        if (movie == null) {
+            throw new MovieNotFoundException("No movie found for imdbId: " + imdbId);
+        }
+        return movie;
     }
 
 }
